1e7bc2c7f7929b844e0b820cdf222b812ac183ba
Joachim <47549862+Schokokex@users.noreply.github.com>
2020-12-19 02:00:52 +0100

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,6 @@
+const asd = new Asd()
+
+
+asd.on(Update, update.filter(wellformed).forEach( return update.data))
+asd.on(Data, update.filter(wellformed).forEach( return update.data))
+// express route into asd

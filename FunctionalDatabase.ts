b69e6e5488ee760d7f28dde506bfdc11e65187b3
Joachim <47549862+Schokokex@users.noreply.github.com>
2020-12-19 01:56:23 +0100

    Create FunctionalDatabase.ts

---
+++ FunctionalDatabase.ts
@@ -0,0 +1,26 @@
+export type DatabaseFunction = {
+}
+
+export class DataModel<T> {
+}
+
+
+export default class FunctionalDatabase {
+  constructor(url: string){
+    
+  }
+  
+  createDataModel<T>(): DataModel<T> {
+  
+  }
+  
+  select(cols: string[], from:){
+  
+  }
+  
+  insert(){
+  
+  }
+  
+  
+}
